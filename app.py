a701ea4defa66800fd3447cb39863a62d271f9f0
icelilis <icelilisrd@gmail.com>
2025-06-30 15:34:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def whatsapp_reply():
     return str(resp)
 
 if __name__ == "__main__":
-app.run(host="0.0.0.0",port=int(os.environ.get("PORT",5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
